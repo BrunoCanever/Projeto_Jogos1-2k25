54fc502efa2b8b723fca2ab956946701670802a0
Bruno Canever <Bbrnocks@gmail.com>
2025-09-09 15:40:04 -0300

    andar

--- Projeto_Jogos1-2k25/Assets/Scripts/Player.cs
+++ Projeto_Jogos1-2k25/Assets/Scripts/Player.cs
@@ -2,15 +2,67 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Animator anim;
+    
+    private Rigidbody2D rigid;
+    public float speed;
+
+    public float jumpforce = 5f;
+    public bool isground;    
+    
     void Start()
     {
+        anim=GetComponent<Animator>();
+        rigid=GetComponent<Rigidbody2D>();
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        Jump();
     }
+    
+    void Move()
+    {
+        float teclas = Input.GetAxis("Horizontal");
+        rigid.linearVelocity = new Vector2(teclas * speed, rigid.linearVelocity.y);
+        if (teclas > 0 && isground == true)
+        {
+            transform.eulerAngles = new Vector2(0, 0);
+            anim.SetInteger("transition", 1);
+
+        }
+        if (teclas < 0 && isground == true)
+        {
+            transform.eulerAngles = new Vector2(0, 180);
+            anim.SetInteger("transition", 1);
+
+        }
+        if (teclas == 0 && isground == true)
+        {
+            anim.SetInteger("transition", 0);
+        }
+
+    }
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.W) && isground==true)
+        {
+            rigid.AddForce(Vector2.up * jumpforce,ForceMode2D.Impulse);
+            anim.SetInteger("transition", 2);
+            isground = false;
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag=="tagGround")
+        {
+            isground = true;
+            Debug.Log("esta no chão,2");
+        }
+    }
+
 }
